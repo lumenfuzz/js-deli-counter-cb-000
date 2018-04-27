215387f9a216370f6479e4429f58b6ee15c9d966
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 21:57:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(name)
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
+
+function nowServing(array) {
+  if (array === []) {
+    return `There is nobody waiting to be served!`
+  }
+  var person = array.pop()
+  return `Currently serving ${person}.`
+}
