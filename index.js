cc6b6a5e07a6119fbbd8a84b96d535ee80024ca6
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 22:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ function takeANumber(katzDeli, name) {
 }
 
 function nowServing(array) {
-  if (array.length < 1) {
+  if (array.length === 0) {
     return `There is nobody waiting to be served!`
   }
   var person = array.shift()
   return `Currently serving ${person}.`
+}
+
+function currentLine(line) {
+  if (array.length === 0) {
+    return `The line is currently empty.`
+  }
+  var string = `1. ${line[0]}`
+  for (var i = 1; i < line.length; i++) {
+    string += `, ${i+1}. ${line[i]}`
+  }
+  return `The line is currently: ${string}`
 }
